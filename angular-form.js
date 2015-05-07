eebf147fa365eb818a79c5dbbacdd74281ea4f54
ruraj.joshi@rts.com.np <ruraj.joshi@rts.com.np>
2015-05-07 19:44:09 +0545

    Templates url taken relative to module js file url, used md-select instead of select

--- angular-form.js
+++ angular-form.js
@@ -2,16 +2,20 @@
  * Created by sagun on 4/15/15.
  */
 
-angular.module('dnForm', [])
+var scripts = document.getElementsByTagName("script");
+var currentScriptPath = scripts[scripts.length-1].src;
+currentScriptPath = currentScriptPath.substring(0, currentScriptPath.lastIndexOf('/') + 1);
+
+angular.module('angularForm', [])
 
     .directive('dnInput', function() {
       return {
         restrict: 'E',
         scope: true,
-        templateUrl: currentScriptPath.substring(0, currentScriptPath.lastIndexOf('/') + 1) + "dn-input.html",
+        templateUrl: currentScriptPath + "angular-input.html",
         replace: true,
         controller: ['$scope', '$element', function($scope, $element) {
-
+          $scope.messagesPath = currentScriptPath + "error-messages.html"
         }]
       }
 
@@ -20,7 +24,7 @@ angular.module('dnForm', [])
     .directive('dnForm', function() {
       return {
         restrict: 'E',
-        templateUrl: currentScriptPath.substring(0, currentScriptPath.lastIndexOf('/') + 1) + "dn-form.html",
+        templateUrl: currentScriptPath + "angular-form.html",
         replace: true,
         controller: ['$scope', '$element', function($scope, $element) {
 
